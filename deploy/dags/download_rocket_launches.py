edc343fa8afb847ae087fca4093c2dac69cc0764
FrankfurtOgunfunminiyi <7263129+frankfurtmacmoses@users.noreply.github.com>
2023-06-02 07:30:53 -0500

    Update download_rocket_launches.py

--- deploy/dags/download_rocket_launches.py
+++ deploy/dags/download_rocket_launches.py
@@ -1,22 +1,24 @@
-import datetime as dt
-from pathlib import Path
-
-import pandas as pd
+import airflow.utils.dates
 from airflow import DAG
-from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 dag = DAG(
-    dag_id="04_time_delta",
-    schedule_interval=dt.timedelta(days=3),
-    start_date=dt.datetime(year=2019, month=1, day=1),
-    end_date=dt.datetime(year=2019, month=1, day=5),
+    dag_id="listing_4_08",
+    start_date=airflow.utils.dates.days_ago(3),
+    schedule_interval="@daily",
 )
 
-fetch_events = BashOperator(
-    task_id="fetch_events",
-    bash_command=(
-       curl google.com
-    ),
-    dag=dag,
+
+def _print_context(**context):
+    start = context["execution_date"]
+    end = context["next_execution_date"]
+    print(f"Start: {start}, end: {end}")
+
+
+# Prints e.g.:
+# Start: 2019-07-13T14:00:00+00:00, end: 2019-07-13T15:00:00+00:00
+
+
+print_context = PythonOperator(
+    task_id="print_context", python_callable=_print_context, dag=dag
 )
